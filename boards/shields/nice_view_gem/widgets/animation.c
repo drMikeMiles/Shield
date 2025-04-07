774a518168067855c5d8cca4e73a60bcd8858d12
drMikeMiles <66922497+drMikeMiles@users.noreply.github.com>
2025-04-08 02:43:18 +0300

    Update animation.c

--- boards/shields/nice_view_gem/widgets/animation.c
+++ boards/shields/nice_view_gem/widgets/animation.c
@@ -3,19 +3,17 @@
 #include "animation.h"
 
 LV_IMG_DECLARE(crystal_01);
-LV_IMG_DECLARE(crystal_02);
 
 const lv_img_dsc_t *anim_imgs[] = {
     &crystal_01,
-    &crystal_02,
 };
 
 void draw_animation(lv_obj_t *canvas) {
 #if IS_ENABLED(CONFIG_NICE_VIEW_GEM_ANIMATION)
     lv_obj_t *art = lv_animimg_create(canvas);
-    lv_obj_center(art, LV_ALIGN_BOTTOM_LEFT, 0, 0);
+    lv_obj_center(art);
 
-    lv_animimg_set_src(art, (const void **)anim_imgs, 2);
+    lv_animimg_set_src(art, (const void **)anim_imgs, 1);
     lv_animimg_set_duration(art, CONFIG_NICE_VIEW_GEM_ANIMATION_MS);
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
     lv_animimg_start(art);
@@ -27,6 +25,5 @@ void draw_animation(lv_obj_t *canvas) {
     int random_index = rand() % length;
 
     lv_img_set_src(art, anim_imgs[random_index]);
-    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
 #endif
 }

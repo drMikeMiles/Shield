dcc64757eb7ae41789988db0bbf1b8c0f239acff
drMikeMiles <66922497+drMikeMiles@users.noreply.github.com>
2025-04-08 02:40:13 +0300

    Update animation.c

--- boards/shields/nice_view_gem/widgets/animation.c
+++ boards/shields/nice_view_gem/widgets/animation.c
@@ -13,7 +13,7 @@ const lv_img_dsc_t *anim_imgs[] = {
 void draw_animation(lv_obj_t *canvas) {
 #if IS_ENABLED(CONFIG_NICE_VIEW_GEM_ANIMATION)
     lv_obj_t *art = lv_animimg_create(canvas);
-    lv_obj_center(art);
+    lv_obj_center(art, LV_ALIGN_BOTTOM_LEFT, 0, 0);
 
     lv_animimg_set_src(art, (const void **)anim_imgs, 2);
     lv_animimg_set_duration(art, CONFIG_NICE_VIEW_GEM_ANIMATION_MS);
@@ -27,6 +27,6 @@ void draw_animation(lv_obj_t *canvas) {
     int random_index = rand() % length;
 
     lv_img_set_src(art, anim_imgs[random_index]);
-    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 40, 40);
+    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
 #endif
 }

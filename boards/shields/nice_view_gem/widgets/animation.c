287a13e3c64b4ee8020976c4cb80a8378b774999
drMikeMiles <66922497+drMikeMiles@users.noreply.github.com>
2025-04-07 22:38:45 +0300

    Update animation.c

--- boards/shields/nice_view_gem/widgets/animation.c
+++ boards/shields/nice_view_gem/widgets/animation.c
@@ -65,3 +65,26 @@ const lv_img_dsc_t *anim_imgs[] = {
     &hammerbeam29,
     &hammerbeam30,
 };
+int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
+    widget->obj = lv_obj_create(parent);
+    lv_obj_set_size(widget->obj, 160, 68);
+    lv_obj_t *top = lv_canvas_create(widget->obj);
+    lv_obj_align(top, LV_ALIGN_TOP_RIGHT, 0, 0);
+    lv_canvas_set_buffer(top, widget->cbuf, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
+
+    lv_obj_t * art = lv_animimg_create(widget->obj);
+    lv_obj_center(art);
+    lv_animimg_set_src(art, (const void **) anim_imgs, 30);
+    lv_animimg_set_duration(art, CONFIG_CUSTOM_ANIMATION_SPEED);
+    lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
+    lv_animimg_start(art);
+    
+    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
+    sys_slist_append(&widgets, &widget->node);
+    widget_battery_status_init();
+    widget_peripheral_status_init();
+
+    return 0;
+}
+
+lv_obj_t *zmk_widget_status_obj(struct zmk_widget_status *widget) { return widget->obj; }

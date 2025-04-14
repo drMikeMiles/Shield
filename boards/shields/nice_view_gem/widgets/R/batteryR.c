0c066d22b33d8f75a14a7259efab10992b3013f9
drMikeMiles <66922497+drMikeMiles@users.noreply.github.com>
2025-04-14 03:35:02 +0300

    UPD

--- boards/shields/nice_view_gem/widgets/R/batteryR.c
+++ boards/shields/nice_view_gem/widgets/R/batteryR.c
@@ -1,5 +1,5 @@
 #include <zephyr/kernel.h>
-#include "battery.h"
+#include "batteryR.h"
 #include "../../assets/custom_fonts.h"
 
 LV_IMG_DECLARE(bolt);
--- boards/shields/nice_view_gem/widgets/R/outputR.c
+++ boards/shields/nice_view_gem/widgets/R/outputR.c
@@ -1,5 +1,5 @@
 #include <zephyr/kernel.h>
-#include "output.h"
+#include "outputR.h"
 #include "../../assets/custom_fonts.h"
 
 LV_IMG_DECLARE(bt_no_signal);

6e579172baa309c7cdc970cb45cf4f650e791ed7
drMikeMiles <66922497+drMikeMiles@users.noreply.github.com>
2025-04-16 01:06:21 +0300

    UPD

---
+++ boards/shields/nice_view_gem/widgets/L/battery.c
@@ -0,0 +1,36 @@
+#include <zephyr/kernel.h>
+#include "battery.h"
+#include "../../assets/custom_fonts.h"
+
+LV_IMG_DECLARE(bolt);
+
+static void draw_level(lv_obj_t *canvas, const struct status_state *state) {
+    lv_draw_label_dsc_t label_left_dsc;
+    init_label_dsc(&label_left_dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_LEFT);
+
+    char text[10] = {};
+
+    sprintf(text, "%i%%", state->battery);
+    lv_canvas_draw_text(canvas, 2, 1, 35, &label_left_dsc, text);
+}
+
+static void draw_charging_level(lv_obj_t *canvas, const struct status_state *state) {
+    lv_draw_img_dsc_t img_dsc;
+    lv_draw_img_dsc_init(&img_dsc);
+    lv_draw_label_dsc_t label_left_dsc;
+    init_label_dsc(&label_left_dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_LEFT);
+
+    char text[10] = {};
+
+    sprintf(text, "%i%%", state->battery);
+    lv_canvas_draw_text(canvas, 2, 1, 35, &label_left_dsc, text);
+    lv_canvas_draw_img(canvas, 35, 3, &bolt, &img_dsc);
+}
+
+void draw_battery_status(lv_obj_t *canvas, const struct status_state *state) {
+    if (state->charging) {
+        draw_charging_level(canvas, state);
+    } else {
+        draw_level(canvas, state);
+    }
+}

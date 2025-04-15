d60346fe6b03f5b55e60a79b35d88ac5ffcd5bb4
drMikeMiles <66922497+drMikeMiles@users.noreply.github.com>
2025-04-15 21:30:58 +0300

    UPD

--- boards/shields/nice_view_gem/custom_status_screen.c
+++ boards/shields/nice_view_gem/custom_status_screen.c
@@ -5,27 +5,14 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include "assets/pixel_operator_mono.c"
 #include "assets/custom_fonts.h"
-#include "widgets/L/hid_indicators.h"
 
 #if IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_STATUS)
 static struct zmk_widget_screen screen_widget;
-static struct zmk_widget_hid_indicators hid_indicators_widget;
 #endif
-#if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
-
-#endif
-
-lv_style_t global_style;
 
 lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *screen;
     screen = lv_obj_create(NULL);
-	
-	
-    zmk_widget_hid_indicators_init(&hid_indicators_widget, screen);
-    lv_obj_align_to(zmk_widget_hid_indicators_obj(&hid_indicators_widget), zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_OUT_TOP_LEFT, 0, -2);
-    
-	
 
 #if IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_STATUS)
     zmk_widget_screen_init(&screen_widget, screen);
